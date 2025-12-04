d90a500fe0a4f5d1ef15e97c10c0b1af111e3cf1
CauaNanis <caua.nanis.rosa2009@gmail.com>
2025-12-03 23:15:24 -0300

    home

--- front_end/index.php
+++ front_end/index.php
@@ -18,23 +18,21 @@ include '../includes/navbar.php';
 </head>
 <body>
 
-    <div class="container">
+    <div class="container-inicio">
 
         <div class="text-box">
             <h2>Bem-vindo à am3nic</h2>
 
-            <p>A am3nic é especializada em peças de hardware que unem qualidade, desempenho e preço justo. Aqui você encontra tudo para montar, atualizar ou melhorar seu PC com confiança: placas-mãe, processadores, memórias, SSDs, placas de vídeo e muito mais.</p>
+            <p>A am3nic é especializada em peças de hardware que unem qualidade, desempenho e preço justo. Aqui você encontra tudo para montar, atualizar ou melhorar seu PC com confiança: placas-mãe, processadores, memórias, SSDs, placas de vídeo e muito mais.
 
-            <p>Trabalhamos com produtos selecionados para garantir potência, durabilidade e compatibilidade. Seja para uma setup gamer, trabalho ou uso diário, estamos prontos para ajudar você a montar a máquina ideal.</p>
-
-            <p>am3nic, tecnologia que acompanha seu ritmo! <br>
-            Quer saber mais sobre nós? <a href="../front_end/sobre.php">clique aqui</a></p>
-
-            <a href="#" class="btn">EXPLORAR</a>
+            Trabalhamos com produtos selecionados para garantir potência, durabilidade e compatibilidade. Seja para uma setup gamer, trabalho ou uso diário, estamos prontos para ajudar você a montar a máquina ideal.
+            am3nic, tecnologia que acompanha seu ritmo! <br>
+            Quer saber mais sobre nós? <a href="../front_end/sobre.php" class="link">clique aqui</a></p>
+            <a href="../front_end/postagens.php" class="btn">EXPLORAR</a>
         </div>
 
         <div class="image-box">
-            <img src="https://i.ibb.co/T0KXgzL/pc-gamer.png" alt="PC Gamer">
+            <img src="../assets/pc.png" alt="PC Gamer">
         </div>
 
     </div>
